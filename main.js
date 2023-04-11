07648a60e05294852044b95c4a815fd153f39b78
GastlyVirus <gstnedgrdrch93@gmail.com>
2023-04-11 11:28:31 -0300

    Se agrego explicacion a todos los campos necesarios y en la version movil se cambio el background del contenedor a negro

--- main.js
+++ main.js
@@ -1,30 +1,29 @@
-let w = (c.width = window.innerWidth),
-  h = (c.height = window.innerHeight),
-  ctx = c.getContext("2d"),
-  minDist = 10,
-  maxDist = 30,
-  initialWidth = 10,
-  maxLines = 200,
-  initialLines = 4,
-  speed = 5,
-  lines = [],
-  frame = 0,
-  timeSinceLast = 0,
+// Esta función en JavaScript crea una animación de líneas en un elemento HTML <canvas>
+let w = (c.width = window.innerWidth), // w: Representa el ancho del canvas y se le asigna el valor de window.innerWidth, que es el ancho de la ventana del navegador.
+  h = (c.height = window.innerHeight), // h: Representa la altura del canvas y se le asigna el valor de window.innerHeight, que es la altura de la ventana del navegador.
+  ctx = c.getContext("2d"), // ctx: Es el contexto de dibujo en 2D del canvas.
+  minDist = 10, // minDist: Es la distancia mínima entre las líneas.
+  maxDist = 30, // maxDist: Es la distancia máxima entre las líneas.
+  initialWidth = 10, // initialWidth: Es el ancho inicial de las líneas.
+  maxLines = 200, // maxLines: Es el número máximo de líneas en la animación.
+  initialLines = 4, // initialLines: Es el número inicial de líneas en la animación.
+  speed = 5, // speed: Es la velocidad de movimiento de las líneas.
+  lines = [], // lines: Es un arreglo que almacenará las instancias de la clase Line que representan las líneas en la animación.
+  frame = 0, // frame: Es un contador de cuadros de animación.
+  timeSinceLast = 0, // timeSinceLast: Es un contador de tiempo desde la última vez que se creó una nueva línea.
   dirs = [
-    // straight x, y velocity
+    // dirs: Es un arreglo que contiene las direcciones posibles de movimiento de las líneas en forma de pares de coordenadas x, y.
     [0, 1],
     [1, 0],
     [0, -1],
     [-1, 0],
-    // diagonals, 0.7 = sin(PI/4) = cos(PI/4)
+
     [0.7, 0.7],
     [0.7, -0.7],
     [-0.7, 0.7],
     [-0.7, -0.7],
   ],
-
-  starter = {
-    // starting parent line, just a pseudo line
+  starter = { // Se define un objeto llamado starter que representa la línea inicial en el centro del canvas, con propiedades de posición (x e y), velocidad (vx y vy) y ancho (width).
     x: w / 2,
     y: h / 2,
     vx: 0,
@@ -32,52 +31,42 @@ let w = (c.width = window.innerWidth),
     width: initialWidth,
   };
 
-function init() {
+
+//----------------------------------------------------------------------------------------------
+
+
+// Se define una función llamada init() que inicializa la animación. Esta función establece el color de fondo del canvas, limpia el canvas y crea un número inicial de líneas con base en el objeto starter.
+function init() { 
   lines.length = 0;
-
   for (var i = 0; i < initialLines; ++i) lines.push(new Line(starter));
-
   ctx.fillStyle = "black";
   ctx.fillRect(0, 0, w, h);
-
-  // if you want a cookie ;)
-  // ctx.lineCap = 'round';
 }
 
 
+// Se define una función llamada getColor(x) que calcula el color de las líneas en función de su posición en el eje x y el contador de cuadros de animación.
 function getColor(x) {
   return "hsl( hue, 100%, 50% )".replace("hue", (x / w) * 360 + frame);
 }
 
 
+// Se define una función llamada anim() que es la función de animación principal. Esta función se ejecuta recursivamente utilizando window.requestAnimationFrame() para realizar una animación suave. En cada cuadro de animación, se actualiza el contador de cuadros (frame), se limpia el canvas con un color de fondo ligeramente transparente, se dibujan y actualizan las líneas existentes, se crean nuevas líneas en intervalos aleatorios, y se ajusta el tamaño del canvas y se reinicializa la animación en caso de cambio de tamaño de la ventana del navegador.
 function anim() {
   window.requestAnimationFrame(anim);
-
   ++frame;
-
   ctx.shadowBlur = 0;
   ctx.fillStyle = "rgba(0,0,0,0.07)";
   ctx.fillRect(0, 0, w, h);
   ctx.shadowBlur = 0.9;
-
   for (var i = 0; i < lines.length; ++i)
     if (lines[i].step()) {
-      // if true it's dead
-
       lines.splice(i, 1);
       --i;
     }
-
-  // spawn new
-
   ++timeSinceLast;
-
   if (lines.length < maxLines && timeSinceLast > 10 && Math.random() < 0.5) {
     timeSinceLast = 0;
-
     lines.push(new Line(starter));
-
-    // cover the middle;
     ctx.fillStyle = ctx.shadowColor = getColor(starter.x);
     ctx.beginPath();
     ctx.arc(starter.x, starter.y, initialWidth, 0, Math.PI * 2);
@@ -86,6 +75,13 @@ function anim() {
 }
 
 
+// La función en cuestión es una definición de una clase llamada "Line" en JavaScript. La clase "Line" tiene un constructor que toma un objeto "parent" como parámetro. La función del constructor es inicializar las propiedades de un objeto "Line" basadas en las propiedades del objeto "parent" y algunos cálculos aleatorios. Las propiedades que se inicializan son:
+
+// "this.x": Se establece como el valor de "parent.x" o como 0 si "parent.x" no está definido.
+// "this.y": Se establece como el valor de "parent.y" o como 0 si "parent.y" no está definido.
+// "this.width": Se establece como el valor de "parent.width" dividido por 1.25.
+
+// Luego, la función utiliza un bucle "do-while" para generar aleatoriamente los valores de "this.vx" y "this.vy" (velocidades en las coordenadas x e y respectivamente) basados en un arreglo de direcciones llamado "dirs". El arreglo "dirs" parece contener pares de números que representan posibles direcciones de movimiento en las coordenadas x e y. El bucle "do-while" se repite mientras "this.vx" y "this.vy" son iguales a las velocidades de "parent" o son opuestas a las velocidades de "parent". Después, las propiedades "this.vx" y "this.vy" se multiplican por una variable llamada "speed", que probablemente es una constante definida en otro lugar en el código. Finalmente, la propiedad "this.dist" se establece como un valor aleatorio entre "minDist" y "maxDist" utilizando la función "Math.random()" de JavaScript, que genera un número aleatorio entre 0 y 1, y luego se escala y desplaza a la escala deseada. "minDist" y "maxDist" también deben ser constantes definidas en otro lugar en el código.
 function Line(parent) {
   this.x = parent.x | 0;
   this.y = parent.y | 0;
@@ -107,34 +103,23 @@ function Line(parent) {
 }
 
 
+// La función "Line.prototype.step" es un método que se añade al prototipo de la clase "Line" definida anteriormente. Este método define el comportamiento de los objetos "Line" en cada paso de la animación. El método "step" realiza las siguientes acciones: Declara una variable booleana llamada "dead" y la inicializa como "false". Esta variable se utiliza para indicar si el objeto "Line" ha alcanzado un estado de "muerte" en la animación. Guarda las coordenadas actuales del objeto "Line" en las variables "prevX" y "prevY", que se utilizan para dibujar una línea desde las coordenadas previas a las coordenadas actuales en el lienzo de dibujo. Actualiza las coordenadas del objeto "Line" sumando las velocidades "this.vx" y "this.vy" a las coordenadas actuales "this.x" y "this.y", respectivamente, lo que produce un movimiento en la dirección indicada por las velocidades. Resta 1 a la propiedad "this.dist" del objeto "Line", que se utiliza para contar la distancia que el objeto ha recorrido en su movimiento. Comprueba si el objeto "Line" ha salido de los límites del lienzo de dibujo. Si es así (si "this.x" es menor que 0, mayor que "w" que parece ser la anchura del lienzo, o si "this.y" es menor que 0 o mayor que "h" que parece ser la altura del lienzo), se establece "dead" como "true". Comprueba si la propiedad "this.dist" del objeto "Line" ha alcanzado 0 y si el ancho "this.width" del objeto "Line" es mayor que 1. Si se cumple esta condición, se genera un nuevo valor aleatorio para "this.dist" entre "minDist" y "maxDist". Luego, se añaden dos nuevos objetos "Line" a la matriz "lines" (que parece ser un arreglo de objetos "Line") utilizando el constructor "Line(this)" con "this" como argumento. Además, hay una probabilidad del 50% de añadir un tercer objeto "Line" a "lines" si la función "Math.random()" devuelve un valor menor que 0.5. Por último, hay una probabilidad del 20% de establecer "dead" como "true".
+// Se establece el estilo de trazo del lienzo de dibujo "ctx" utilizando la función "getColor(this.x)" para obtener un color basado en las coordenadas x actuales del objeto "Line". Luego, se inicia un nuevo trazo en el lienzo utilizando "ctx.beginPath()", se establece el ancho de línea como "this.width" utilizando "ctx.lineWidth", se mueve el puntero de dibujo a las coordenadas actuales del objeto "Line" utilizando "ctx.moveTo(this.x, this.y)", y se dibuja una línea hasta las coordenadas previas utilizando "ctx.lineTo(prevX, prevY)". Por último, se realiza el trazo en el lienzo utilizando "ctx.stroke()". Si "dead" es "true", se retorna "true" para indicar que el objeto "Line" ha alcanzado un estado de "muerte" en la animación. En caso contrario, no se retorna nada (implícitamente se retorna "undefined").
 Line.prototype.step = function () {
   var dead = false;
-
   var prevX = this.x,
     prevY = this.y;
-
   this.x += this.vx;
   this.y += this.vy;
-
   --this.dist;
-
-  // kill if out of screen
   if (this.x < 0 || this.x > w || this.y < 0 || this.y > h) dead = true;
-
-  // make children :D
   if (this.dist <= 0 && this.width > 1) {
-    // keep yo self, sometimes
     this.dist = Math.random() * (maxDist - minDist) + minDist;
-
-    // add 2 children
     if (lines.length < maxLines) lines.push(new Line(this));
     if (lines.length < maxLines && Math.random() < 0.5)
       lines.push(new Line(this));
-
-    // kill the poor thing
     if (Math.random() < 0.2) dead = true;
   }
-
   ctx.strokeStyle = ctx.shadowColor = getColor(this.x);
   ctx.beginPath();
   ctx.lineWidth = this.width;
@@ -145,9 +130,17 @@ Line.prototype.step = function () {
   if (dead) return true;
 };
 
+
 init();
 anim();
 
+
+// esta función se ejecuta cuando se redimensiona la ventana del navegador y actualiza el ancho, altura y posición de inicio de las líneas en la animación en función del nuevo tamaño de la ventana. 
+// Asigna el valor de "window.innerWidth" a la variable "w", que representa el ancho del lienzo de dibujo "c". Esto actualiza el ancho del lienzo al ancho de la ventana del navegador.
+// Asigna el valor de "window.innerHeight" a la variable "h", que representa la altura del lienzo de dibujo "c". Esto actualiza la altura del lienzo a la altura de la ventana del navegador.
+// Asigna el valor de "w / 2" a la propiedad "x" del objeto "starter", que es un objeto de la clase "Line" utilizado como punto de inicio para las nuevas líneas en la animación. Esto coloca el objeto "starter" en la mitad del ancho del lienzo después de la redimensión de la ventana.
+// Asigna el valor de "h / 2" a la propiedad "y" del objeto "starter", que es el punto de inicio en la altura para las nuevas líneas en la animación. Esto coloca el objeto "starter" en la mitad de la altura del lienzo después de la redimensión de la ventana.
+// Llama a la función "init()" para reiniciar la animación con los nuevos valores de ancho y altura del lienzo.
 window.addEventListener("resize", function () {
   w = c.width = window.innerWidth;
   h = c.height = window.innerHeight;
